bf44e13f8fb3d471d42ac2dfa6d4c2146336082f
Thiagoht10 <thiagodesouzaht@gmail.com>
2026-09-14 19:14:14 +0100

    fixing cast return in base.cpp

--- cpp_06/ex02/Base.cpp
+++ cpp_06/ex02/Base.cpp
@@ -34,7 +34,7 @@ void    identify(Base& p)
 {
     try
     {
-        dynamic_cast<A&>(p);
+        (void)dynamic_cast<A&>(p);
         std::cout << "A" << std::endl;
     }
     catch(const std::exception& e)
@@ -42,7 +42,7 @@ void    identify(Base& p)
 
     try
     {
-        dynamic_cast<B&>(p);
+        (void)dynamic_cast<B&>(p);
         std::cout << "B" << std::endl;
     }
     catch(const std::exception& e)
@@ -50,7 +50,7 @@ void    identify(Base& p)
 
     try
     {
-        dynamic_cast<C&>(p);
+        (void)dynamic_cast<C&>(p);
         std::cout << "C" << std::endl;
     }
     catch(const std::exception& e)
